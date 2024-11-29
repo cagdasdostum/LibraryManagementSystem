0f736c7c505e277086c91972a04c3f8774c699be
cagdasdostum <cagdascalayir@outlook.com.tr>
2024-11-29 19:56:51 +0300

    Üye sınıfı oluşturuldu ve istenenlere göre içi dolduruldu.

---
+++ src/Member.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class Member {
+
+    private String name;
+    private int memberID;
+    ArrayList<Book> borrowedBooks;
+
+    public Member(String name, int memberID, ArrayList<Book> borrowedBooks) {
+        this.name = name;
+        this.memberID = memberID;
+        this.borrowedBooks = borrowedBooks;
+
+    }
+
+    public void BorrowBook(Book book) {
+        if (borrowedBooks.size() < 5) {
+            borrowedBooks.add(book);
+            System.out.println("Kitap ödünç alındı.");
+        }
+        else {
+            System.out.println("Kitap ödünç alma limitini aştınız.");
+        }
+    }
+
+    public void ReturnBook(Book book) {
+        borrowedBooks.remove(book);
+        System.out.println("Kitap iade edildi.");
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "name='" + name + '\'' +
+                ", memberID=" + memberID +
+                ", borrowedBooks=" + borrowedBooks +
+                '}';
+    }
+}
